8dae74cd90d06983afded2741c91b407d23048b1
Eric <eric.so.dev@gmail.com>
2025-07-20 23:27:32 -0400

    test: basic lexer tests

--- tests/lexer.test.ts
+++ tests/lexer.test.ts
@@ -10,7 +10,32 @@ import {
 } from "../src/parser/tokens";
 
 describe("Test tokenizer", () => {
-  it("Kitchen sink", () => {
+  test.each([
+    ["word", [new StringToken("word")]],
+    [
+      "a sentence.",
+      [new StringToken("a"), new SpaceToken(), new StringToken("sentence.")],
+    ],
+    [
+      "# heading",
+      [new HashToken(), new SpaceToken(), new StringToken("heading")],
+    ],
+    [
+      "multiple\nlines",
+      [
+        new StringToken("multiple"),
+        new LineBreakToken(),
+        new StringToken("lines"),
+      ],
+    ],
+  ])("Basic tests", (input, tokens) => {
+    const expectedTokens = [...tokens, new EOFToken()];
+    const tokenizer = new Tokenizer(input);
+    const output = tokenizer.tokenize();
+
+    expect(output).toEqual(expectedTokens);
+  });
+  test("Kitchen sink", () => {
     const input = "one two three four **foo** *bar* # four \n 5 six";
     const tokenizer = new Tokenizer(input);
     const output = tokenizer.tokenize();
